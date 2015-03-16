86b6a633d6652e64c7bf31c46cb6d0a69c7a5f40
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-03-16 12:24:05 +0100

    Make into command-line script for now

--- vtk_renderer.py
+++ vtk_renderer.py
@@ -1,15 +1,17 @@
-import sys
+import argparse
+
 import vtk
 import numpy as np
-from astropy.io import fits
 from PyQt4 import QtGui
 from vtk.qt4.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
+
 from palettable.colorbrewer import get_map
 
+from spectral_cube import SpectralCube
 
 class QtVTKRenderer(QtGui.QWidget):
 
-    def __init__(self, data, vmin=None, vmax=None, levels=[]):
+    def __init__(self, data, vmin=None, vmax=None, levels=[], spectral_stretch=1.):
 
         super(QtVTKRenderer, self).__init__()
 
@@ -22,7 +24,7 @@ class QtVTKRenderer(QtGui.QWidget):
         data = np.clip((data - vmin) / (vmax - vmin) * 255., 0., 255.)
         data = data.astype(np.uint8)
         data_string = data.tostring()
-        
+
         # Prepare levels
         levels = np.asarray(levels)
         levels = np.clip((levels - vmin) / (vmax - vmin) * 255., 0., 255.)
@@ -33,7 +35,7 @@ class QtVTKRenderer(QtGui.QWidget):
         self.readerVolume.SetNumberOfScalarComponents(1)
         self.readerVolume.SetDataExtent(0, nx - 1, 0, ny - 1, 0, nz - 1)
         self.readerVolume.SetWholeExtent(0, nx - 1, 0, ny - 1, 0, nz - 1)
-        self.readerVolume.SetDataSpacing(1, 1, 1)
+        self.readerVolume.SetDataSpacing(1, 1, spectral_stretch)
 
         self.ren = vtk.vtkRenderer()
         self.ren.SetBackground(0, 0, 0)
@@ -47,10 +49,10 @@ class QtVTKRenderer(QtGui.QWidget):
         self.vtkw.resize(800, 800)
         self.vtkw.AddObserver("ExitEvent", lambda o, e, a=app: a.quit())
 
-        palette = get_map('RdYlBu', 'diverging', len(levels))
+        palette = get_map('RdYlBu', 'diverging', max(3,len(levels)))
 
         for ilevel, level in enumerate(levels):
-            self.add_contour(level, color=palette.mpl_colors[ilevel], alpha=0.4)
+            self.add_contour(level, color=palette.mpl_colors[ilevel], alpha=0.5)
 
         self.renWin.Render()
         self.renWin.PolygonSmoothingOn()
@@ -81,13 +83,26 @@ class QtVTKRenderer(QtGui.QWidget):
 
 if __name__ == "__main__":
 
-    data = fits.getdata(sys.argv[1])
-    data = data[145:245, :, :]
-    data[data < 0.7] = 0.
+    parser = argparse.ArgumentParser(description='View a spectral cube')
+    parser.add_argument('filename', type=str,
+                       help='the name of the file to read in')
+    parser.add_argument('levels', nargs="+", type=float,
+                       help='the levels to show')
+    parser.add_argument('--stretch-spectral', nargs=1, help='Factor by which to stretch spectral dimension', default=1)
+
+    args = parser.parse_args()
+    if len(args.levels) > 8:
+        raise ValueError("Too many levels (maximum allowed is 8)")
+
+    data = SpectralCube.read(args.filename, format='fits')
 
     app = QtGui.QApplication(['QVTKRenderWindowInteractor'])
 
-    w = QtVTKRenderer(data, vmin=0., vmax=5., levels=[1.0, 2.0, 3.0, 4.0])
+    levels = np.array(args.levels)
+    vmin = levels.min() / 2.
+    vmax = levels.max() * 2.
+
+    w = QtVTKRenderer(data, vmin=vmin, vmax=vmax, levels=levels, spectral_stretch=float(args.stretch_spectral[0]))
     w.show()
 
     app.exec_()
