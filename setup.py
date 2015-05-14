f967fca2935cf9232b349f7eca69fd998ac258cd
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-05-13 21:46:40 -0400

    Make into package

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, Command
+
+
+setup(name='cube-viewer',
+      version='0.0.dev',
+      url='http://www.github.com/astrofrog/cube-viewer.git',
+      description='Simple 3D FITS cube viewer',
+      author='Thomas Robitaille',
+      author_email='thomas.robitaille@gmail.com',
+      packages=['cube_viewer'],
+      package_data={'cube_viewer':['*.ui']},
+      keywords=['Scientific/Engineering'],
+      classifiers=[
+                   "Development Status :: 4 - Beta",
+                   "Programming Language :: Python",
+                   "License :: OSI Approved :: BSD License",
+                  ],
+     )
